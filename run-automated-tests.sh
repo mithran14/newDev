76d9f8ef05585f3d56299af70623a3be1a67859a
mithran14 <30817381+mithran14@users.noreply.github.com>
2023-06-01 19:50:58 +0530

    Update run-automated-tests.sh

--- run-automated-tests.sh
+++ run-automated-tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 # Connect to the EC2 instance
-ssh -i "opencart.pem" bitnami@ec2-13-127-80-239.ap-south-1.compute.amazonaws.com -o StrictHostKeyChecking=no  << EOF
+ssh -i "opencart.pem" -o StrictHostKeyChecking=no bitnami@ec2-13-127-80-239.ap-south-1.compute.amazonaws.com   << EOF
 
 # Update the package lists for upgrades and new package installations
 sudo apt-get update
